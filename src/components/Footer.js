a45a896a949fab062aef1c1d1dcdf91aac341103
EmanAmr1 <emanamr543@gmail.com>
2024-02-28 22:31:14 +0200

    Edit styles in product details

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -68,7 +68,7 @@ const Footer = () => {
             <div class="col-lg-12">
                 
                 <div class="footer__copyright__text">
-                    <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
+                    <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href=" " target="_blank">ITI TEAM</a></p>
                 </div>
                 
             </div>

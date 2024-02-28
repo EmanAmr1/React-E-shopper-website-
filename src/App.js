d1f0ce05f025c239a60eb697c7cc7f8c8f0f26f1
EmanAmr1 <emanamr543@gmail.com>
2024-02-28 21:10:34 +0200

    Header component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'jquery-ui-dist/jquery-ui.min.css';
+import 'magnific-popup/dist/magnific-popup.css';
+import 'owl.carousel/dist/assets/owl.carousel.min.css';
+import 'slicknav/dist/slicknav.min.css';
+
+import Header from './components/Header';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+   <>
+   <Header />
+   </>
   );
 }
 

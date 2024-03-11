2351be28324b8988fa58102a028fde59e00eacc1
Hanaa Gamal <elshriefhanaa@gmail.com>
2024-03-11 02:37:05 +0200

    rating for product

--- src/components/Shop/ListOfProduct.js
+++ src/components/Shop/ListOfProduct.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ProductRating from "./ProductRating";
 
 const ListOfProduct = ({
   products,
@@ -31,7 +32,7 @@ const ListOfProduct = ({
               <ul className="product__hover">
                 <li>
                   <a href={prod.image} className="image-popup">
-                  <a href={`/productDetails/${prod.id}`}>    <span className="arrow_expand"  ></span></a>
+                    <a href={`/productDetails/${prod.id}`}>    <span className="arrow_expand"  ></span></a>
                   </a>
                 </li>
                 <li>
@@ -63,41 +64,37 @@ const ListOfProduct = ({
               <h6>
                 <a href="h">{prod.name}</a>
               </h6>
-              <div className="rating">
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-              </div>
-              {prod.sale ? (
-                <div className="product__price  " style={{ color: "#ca1515" }}>
-                  {prod.newprice} <span>{prod.price}</span>
-                </div>
-              ) : (
-                <div className="product__price">{prod.price}</div>
-              )}
+              <div>
+              <ProductRating rating={prod.ratings} />
             </div>
+            {prod.sale ? (
+              <div className="product__price  " style={{ color: "#ca1515" }}>
+                {prod.newprice} <span>{prod.price}</span>
+              </div>
+            ) : (
+              <div className="product__price">{prod.price}</div>
+            )}
           </div>
         </div>
-      ))}
-      <div class="col-lg-12 text-center mt-3">
-        <div class="pagination__option">
-          {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
-            <a
-              href="#"
-              key={page}
-              onClick={() => handlePageChange(page)}
-              className={`pagination-link ${
-                currentPage === page ? "activee" : ""
-              }`}
-            >
-              {page}
-            </a>
-          ))}
         </div>
-      </div>
-    </div>
+  ))
+}
+<div class="col-lg-12 text-center mt-3">
+  <div class="pagination__option">
+    {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+      <a
+        href="#"
+        key={page}
+        onClick={() => handlePageChange(page)}
+        className={`pagination-link ${currentPage === page ? "activee" : ""
+          }`}
+      >
+        {page}
+      </a>
+    ))}
+  </div>
+</div>
+    </div >
   );
 };
 

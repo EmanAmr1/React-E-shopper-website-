52e87ec54c2ef043e602eba68922179647434eee
“aya <ayahegab2k@gmail.com>
2024-03-16 09:52:31 +0200

    send notification to users when shipped order

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -124,21 +124,20 @@ const Cart = () => {
 
   return (
     <div>
-                      
-    {/* Breadcrumb Begin */}
-    <div className="breadcrumb-option">
-      <div className="container">
-      <div className="row">
-        <div className="col-lg-12">
-          <div className="breadcrumb__links">
-            <Link to="/">
-              <i className="fa fa-home"></i> Home
-            </Link>
-            <span> Shopping cart</span>
+      {/* Breadcrumb Begin */}
+      <div className="breadcrumb-option">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="breadcrumb__links">
+                <Link to="/">
+                  <i className="fa fa-home"></i> Home
+                </Link>
+                <span> Shopping cart</span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      </div>
       </div>
       {/* Breadcrumb End */}
       <section className="h-100">

86619f8fcfd506a4d01d5ffafd8781d4ca270d92
Esraa Abd El_Maksoud <abdelmaksoudesraa99@gmail.com>
2024-03-24 03:37:17 +0200

    add sidebar to customer profile

--- src/components/Accounts/CustomerProfile.js
+++ src/components/Accounts/CustomerProfile.js
@@ -7,19 +7,21 @@ import { useDispatch } from "react-redux";
 import { setTotalCount } from "../../store/slices/total";
 import { resetWishTotalCount } from "../../store/slices/wishlist";
 import { axiosInstance } from "../../apis/config";
+
 // Function to get status text based on status code
 function getStatusText(status) {
   switch (status) {
-    case 'D':
-      return 'Delivered';
-    case 'S':
-      return 'Shipped';
-    case 'P':
-      return 'Pending';
+    case "D":
+      return "Delivered";
+    case "S":
+      return "Shipped";
+    case "P":
+      return "Pending";
     default:
-      return 'Unknown';
+      return "Unknown";
   }
 }
+
 function CustomerProfile() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -35,6 +37,7 @@ function CustomerProfile() {
   });
   const [isModified, setIsModified] = useState(false);
   const [userOrders, setUserOrders] = useState([]);
+  const [showProfile, setShowProfile] = useState(true);
   const token = Cookies.get("token");
   const headers = {
     Authorization: `Token ${token}`,
@@ -54,7 +57,8 @@ function CustomerProfile() {
         dispatch(resetWishTotalCount(res.data.wishlist_items.length));
       })
       .catch((err) => console.log(err));
-      axiosInstance
+
+    axiosInstance
       .get("/API/userorders/", { headers })
       .then((res) => {
         setUserOrders(res.data.orders);
@@ -62,7 +66,6 @@ function CustomerProfile() {
       .catch((error) => {
         console.error("Fetch user orders error:", error);
       });
-
   }, []);
 
   useEffect(() => {
@@ -123,9 +126,7 @@ function CustomerProfile() {
       .then((res) => {
         console.log("Update successful");
         setLoading(false);
-        if (
-          window.confirm("Your profile has been updated")
-        ) {
+        if (window.confirm("Your profile has been updated")) {
           navigate("/login");
         }
       })
@@ -178,156 +179,262 @@ function CustomerProfile() {
       .catch((error) => {
         console.error("Delete error:", error);
       });
-    
-      
   };
-  console.log(userOrders)
+
   return (
-    <div className="container py-5">
+    <div
+      className="container-fluid pl-0"
+      style={{ backgroundColor: "#f3f4f8" }}
+    >
       <div className="row">
         <div className="col-md-3">
-          <div className="card">
-            <div className="card-body">
-              <h4 className="card-title">Welcome {user ? user.first_name : ""}</h4>
-              <h6 className="card-subtitle mb-2 text-muted">
+          <div className="card1">
+            <div className="card-body px-5">
+              <h5 className="card-title font-weight-bold">
+                {user ? user.first_name : ""} !
+              </h5>
+              <h6 className="card-subtitle text-muted">
                 {user ? user.email : ""}
               </h6>
-              <ul className="list-unstyled">
-                <li>
-                  <button className="btn btn-link" style={{ textDecoration: 'none' }}>
-                    <Link to="/VerifyOTP" style={{ textDecoration: 'none' }}>
-                      Change Password
+              <hr />
+              <ul className="list-unstyled" style={{ paddingLeft: 0 }}>
+                <li className="mt-2">
+                  <button
+                    className="btn btn-link sidebar-btn"
+                    style={{
+                      textDecoration: "none",
+                      
+                      textAlign: "start",
+                    }}
+                  >
+                    <Link
+                      to="/VerifyOTP"
+                      style={{ textDecoration: "none", textAlign: "start" }}
+                    >
+                      <i
+                        className="fas fa-key mr-2"
+                        style={{ color: "rgb(227, 192, 28)", fontSize: "20px" }}
+                      ></i>
+                      <span
+                        className="align-text-start"
+                        style={{
+                          fontSize: "17px",
+                          color: "rgb(126, 133, 155)",
+                        }}
+                      >
+                        Change Password
+                      </span>
                     </Link>
                   </button>
                 </li>
-                <li>
+                <li className="mt-2">
                   <button
-                    className="btn btn-link text-danger"
+                    className="btn btn-link sidebar-btn text-danger"
                     onClick={handleDelete}
-                    style={{ textDecoration: 'none' }}
+                    style={{
+                      textDecoration: "none",
+                    
+                      textAlign: "start",
+                    }}
                   >
-                    Delete Account
+                    <i
+                      className="fas fa-trash-alt mr-2"
+                      style={{ color: "rgb(227, 192, 28)", fontSize: "20px" }}
+                    ></i>
+                    <span
+                      className="align-text-start"
+                    
+                      style={{ fontSize: "17px", color: "rgb(126, 133, 155)" }}
+                    >
+                      Delete Account
+                    </span>
+                  </button>
+                </li>
+                <li className="mt-2">
+                  <button
+                    className="btn btn-link sidebar-btn"
+                    onClick={() => setShowProfile(true)}
+                    style={{
+                      textDecoration: "none",
+                     
+                      textAlign: "start",
+                    }}
+                  >
+                    <i
+                      className="fas fa-user mr-2"
+                      style={{ color: "rgb(227, 192, 28)", fontSize: "20px" }}
+                    ></i>
+                    <span
+                      className="align-text-start"
+                      style={{ fontSize: "17px", color: "rgb(126, 133, 155)" }}
+                    >
+                      Profile
+                    </span>
+                  </button>
+                </li>
+                <li className="mt-2">
+                  <button
+                    className="btn btn-link sidebar-btn"
+                    onClick={() => setShowProfile(false)}
+                    style={{
+                      textDecoration: "none",
+                      
+                      textAlign: "start",
+                    }}
+                  >
+                    <i
+                      className="fas fa-list-alt mr-2"
+                      style={{ color: "rgb(227, 192, 28)", fontSize: "20px" }}
+                    ></i>
+                    <span
+                      className="align-text-start"
+                      style={{fontSize: "17px", color: "rgb(126, 133, 155)" }}
+                    >
+                      Orders
+                    </span>
                   </button>
                 </li>
               </ul>
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
             </div>
           </div>
         </div>
-        <div className="col-md-9">
-          <div className="card">
-            <div className="card-body">
-              <form onSubmit={handleUpdate}>
-                <div className="form-group">
-                  <label htmlFor="first_name">First Name:</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="first_name"
-                    name="first_name"
-                    value={updatedUser.first_name}
-                    onChange={handleFieldChange}
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="last_name">Last Name:</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="last_name"
-                    name="last_name"
-                    value={updatedUser.last_name}
-                    onChange={handleFieldChange}
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="address">Address:</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="address"
-                    name="address"
-                    value={updatedUser.address}
-                    onChange={handleFieldChange}
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="phone">Phone Number:</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="phone"
-                    name="phone"
-                    value={updatedUser.phone}
-                    onChange={handleFieldChange}
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="birthdate">Birthdate:</label>
-                  <input
-                    type="date"
-                    className="form-control"
-                    id="birthdate"
-                    name="birthdate"
-                    value={updatedUser.birthdate}
-                    onChange={handleFieldChange}
-                  />
-                </div>
-                <button
-                  type="submit"
-                  className="btn btn-primary"
-                  disabled={!isModified}
-                >
-                  Update
-                </button>
-              </form>
-            </div>
-          </div>
-          <div className="card mt-4">
-  <div className="card-body">
-    <h5 className="card-title">Your Orders</h5>
-    <div className="list-group">
-      {userOrders.map((order) => (
-        <div key={order.id} className="list-group-item">
-          <div className="d-flex w-100 justify-content-between">
-            <h6 className="mb-1">Order Code: {order.id}</h6>
-            <small>Status: {getStatusText(order.status)}</small>
-          </div>
-          <ul className="list-group">
-            {order.orderItems.map((item, index) => (
-              <li key={index} className="list-group-item">
-                <div className="d-flex w-100 justify-content-between">
-                  <h6 className="mb-1">{item.name}</h6>
-                  <span className="badge badge-primary badge-pill">
-                    ${item.price}
+        <div className="col-md-9  py-4">
+          {showProfile ? (
+            <div className="card">
+              <div className="card-body px-5">
+                <form onSubmit={handleUpdate}>
+                  <span style={{ fontWeight: "semibold", fontSize: "20px" }}>
+                    Profile Info
                   </span>
+                  <div className="form-group mt-3">
+                    <label htmlFor="first_name ">First Name:</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="first_name"
+                      name="first_name"
+                      value={updatedUser.first_name}
+                      onChange={handleFieldChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="last_name">Last Name:</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="last_name"
+                      name="last_name"
+                      value={updatedUser.last_name}
+                      onChange={handleFieldChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="address">Address:</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="address"
+                      name="address"
+                      value={updatedUser.address}
+                      onChange={handleFieldChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="phone">Phone Number:</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="phone"
+                      name="phone"
+                      value={updatedUser.phone}
+                      onChange={handleFieldChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="birthdate">Birthdate:</label>
+                    <input
+                      type="date"
+                      className="form-control"
+                      id="birthdate"
+                      name="birthdate"
+                      value={updatedUser.birthdate}
+                      onChange={handleFieldChange}
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="btn btn-primary"
+                    disabled={!isModified}
+                  >
+                    Update
+                  </button>
+                </form>
+              </div>
+            </div>
+          ) : (
+            <div className="card mt-4">
+              <div className="card-body">
+                <h5 className="card-title">Your Orders</h5>
+                <div className="list-group">
+                  {userOrders.map((order) => (
+                    <div key={order.id} className="list-group-item">
+                      <div className="d-flex w-100 justify-content-between">
+                        <h6 className="mb-1">Order Code: {order.id}</h6>
+                        <small>Status: {getStatusText(order.status)}</small>
+                      </div>
+                      <ul className="list-group">
+                        {order.orderItems.map((item, index) => (
+                          <li key={index} className="list-group-item">
+                            <div className="d-flex w-100 justify-content-between">
+                              <h6 className="mb-1">{item.name}</h6>
+                              <span className="badge badge-primary badge-pill">
+                                ${item.price}
+                              </span>
+                            </div>
+                            <small>Quantity: {item.quantity}</small>
+                          </li>
+                        ))}
+                      </ul>
+                      <div className="d-flex w-100 justify-content-between mt-2">
+                        <div>
+                          <strong>Total Price:</strong> ${order.total_price}
+                        </div>
+                        <div>
+                          <button className="btn btn-primary btn-sm">
+                            Track Order
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
                 </div>
-                <small>Quantity: {item.quantity}</small>
-              </li>
-            ))}
-          </ul>
-          <div className="d-flex w-100 justify-content-between mt-2">
-            <div>
-              <strong>Total Price:</strong> ${order.total_price}
+              </div>
             </div>
-            <div>
-              <button className="btn btn-primary btn-sm">
-                Track Order
-              </button>
-            </div>
-          </div>
+          )}
         </div>
-      ))}
+      </div>
+      <hr className="my-0" />
     </div>
-  </div>
-</div>
-
-      </div>
-        </div>
-      </div>
-    
   );
-
-
 }
 
 export default CustomerProfile;

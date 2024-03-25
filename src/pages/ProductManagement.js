2b715630d16fe873a3dba0067c6e82e78efdf1b2
EmanAmr1 <emanamr543@gmail.com>
2024-03-25 17:10:06 +0200

    Enhance productManagement in admin panal

--- src/pages/ProductManagement.js
+++ src/pages/ProductManagement.js
@@ -37,75 +37,58 @@ function ProductManagement() {
     };
 
 
-
-
-
-    /*
-        const handleUpdateProduct = (productId) => {
-            console.log(productId)
-        axiosInstance.put(`/API/updateProduct/${productId}/`, formData, { headers })
-        .then(res => {
-            setUpdatePro(prevState => ({
-                ...prevState,
-                ...res.data.product
-            }));
-            setSuccessMessage('Product successfully updated');
-            setErrors([]);
-    
-        })
-        .catch(error => {
-            console.error('Error updating product:', error);
-            setErrors(['Error updating product']);
-        });
-        }
-    */
-
-
-
     return (
         <>
-            <h3>All Products</h3>
-            <Link to={`/AdminAddPro`} className="btn ">Add</Link>
 
-            <table className="table table-success table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">image</th>
-                        <th scope="col">description</th>
-                        <th scope="col">brand</th>
-                        <th scope="col">category</th>
-                        <th scope="col">ratings</th>
-                        <th scope="col">price</th>
-                        <th scope="col">newprice</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {products.map((prod) => (
-                        <tr key={prod.id} className="table-light">
-                            <td>{prod.id}</td>
-                            <td>{prod.name}</td>
-                            <td><img src={`http://127.0.0.1:8000${prod.image}`} alt={prod.name} style={{ width: '50px', height: '50px' }} /></td>
-                            <td>{prod.description}</td>
-                            <td>{prod.brand}</td>
-                            <td>{prod.category}</td>
-                            <td>{prod.ratings}</td>
-                            <td>{prod.price}</td>
-                            <td>{prod.newprice}</td>
 
-                            <td>
+            <div className="card" style={{ width: "1450px" }}>
+                <div className="card-header bg-dark text-white " >
+                    <h3 className="mb-0 " >All Products</h3>
+                </div>
+                <div className="card-body">
+                    <Link to={`/AdminAddPro`} className="btn btn-info mb-3">Add New Product</Link>
+                    <div className="table-container">
+                        <table className="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Image</th>
+                                    <th>Description</th>
+                                    <th>Brand</th>
+                                    <th>Category</th>
+                                    <th>Ratings</th>
+                                    <th>Price</th>
+                                    <th>New Price</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {products.map((prod) => (
+                                    <tr key={prod.id}>
+                                        <td>{prod.id}</td>
+                                        <td>{prod.name}</td>
+                                        <td>
+                                            <img src={`http://127.0.0.1:8000${prod.image}`} alt={prod.name} style={{ maxWidth: '100px', maxHeight: '100px' }} />
+                                        </td>
+                                        <td>{prod.description}</td>
+                                        <td>{prod.brand}</td>
+                                        <td>{prod.category}</td>
+                                        <td>{prod.ratings}</td>
+                                        <td>{prod.price}</td>
+                                        <td>{prod.newprice}</td>
+                                        <td>
+                                            <button className="btn btn-danger " style={{width:'100px'}} onClick={() => handleDeleteProduct(prod.id)}>Delete</button>
+                                            <Link to={`/AdminUpdatePro/${prod.id}`} className="btn-success mt-2 btn" style={{width:'100px'}}>Update</Link>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
 
-                                <button className="btn btn-danger" onClick={() => handleDeleteProduct(prod.id)}>Delete</button>
-                                <Link to={`/AdminUpdatePro/${prod.id}`} className="btn ">Update</Link>
-                               
-                            </td>
-
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
         </>
     );
 }

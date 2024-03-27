7f9f95710a0cbe584a991ac22833a5c91d9063fe
Taghreed Muhammed <taghreedmuhammed121@gmail.com>
2024-03-27 02:20:42 +0200

    product statistics

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme,VictoryLabel } from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryLabel } from 'victory';
 
 function AdminPanel() {
     const [adminStatistics, setAdminStatistics] = useState({});
     const [orderStatistics, setOrderStatistics] = useState({});
     const [planStatistics, setPlanStatistics] = useState({});
+    const [productStatistics, setProductStatistics] = useState({});
 
     useEffect(() => {
         fetchAdminStatistics();
         fetchOrderStatistics();
         fetchPlanStatistics();
+        fetchProductStatistics();
     }, []);
 
     const fetchAdminStatistics = () => {
@@ -32,6 +34,13 @@ function AdminPanel() {
             .then(response => response.json())
             .then(data => setPlanStatistics(data))
             .catch(error => console.error('Error fetching plan statistics:', error));
+    };
+
+    const fetchProductStatistics = () => {
+        fetch('http://localhost:8000/best-selling-products/')
+            .then(response => response.json())
+            .then(data => setProductStatistics(data))
+            .catch(error => console.error('Error fetching product statistics:', error));
     };
 
     return (
@@ -94,7 +103,7 @@ function AdminPanel() {
 
             <div style={{ display: 'flex', justifyContent: 'center', gap: '20px', marginTop: '20px' }}>
                 <div style={{ width: '50%' }}>
-                    <h2>Admin Statistics</h2>
+                    <h2>Quantity Statistics</h2>
                     <VictoryChart
                         theme={VictoryTheme.material}
                         domainPadding={20}
@@ -176,9 +185,42 @@ function AdminPanel() {
                     </VictoryChart>
                 </div>
             </div>
+
+            <div style={{ display: 'flex', justifyContent: 'center', gap: '20px', marginTop: '20px' }}>
+    {productStatistics.best_selling_products && (
+        <div style={{ width: '50%' }}>
+            <h2>Product Statistics</h2>
+            <VictoryChart
+                theme={VictoryTheme.material}
+                domainPadding={20}
+                width={600}
+                height={400}
+            >
+                <VictoryAxis
+                    tickValues={productStatistics.best_selling_products.map(product => product.product_name)}
+                    tickFormat={tick => tick}
+                    style={{ tickLabels: { fontSize: 12 } }}
+                />
+                <VictoryAxis
+                    dependentAxis
+                    tickFormat={tick => `${tick}`}
+                    style={{ tickLabels: { fontSize: 12 } }}
+                />
+                <VictoryBar
+                    data={productStatistics.best_selling_products}
+                    x="product_name"
+                    y="total_quantity_sold"
+                    labels={({ datum }) => datum.total_quantity_sold}
+                    labelComponent={<VictoryLabel dy={-15} />}
+                    barWidth={40}
+                />
+            </VictoryChart>
+        </div>
+    )}
+</div>
+
         </>
     );
 }
 
 export default AdminPanel;
-
